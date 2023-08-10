2e323494b7a548f1996813d876b27123c24a4ff9
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-08-10 21:51:36 +0200

    This Class Implements the Resources to be Shared

---
+++ Codes/Bakery_Lock---N_Threads/SharedResources.java
@@ -0,0 +1,21 @@
+import java.util.concurrent.locks.Lock;
+// Name: Selepe Sello
+// Student Number: u20748052
+
+public class SharedResources {
+    Lock l;
+	Bakery bakeryLock = new Bakery(5);
+	public void access(int resource_number){
+		this.bakeryLock.lock();
+		try {
+			System.out.println(Thread.currentThread().getName() + ": flag[" + resource_number + "] = " + this.bakeryLock.getflag()[resource_number] + ", label[" + resource_number + "] = " + (this.bakeryLock.getMaximumLabel(this.bakeryLock.getlabel()) + 1));
+			int sleeping_time = (int)(Math.random() * (1000 - 200 + 1) + 200);
+			Thread.sleep(sleeping_time);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		} finally {
+			System.out.println(Thread.currentThread().getName() + " ------------------- DONE");
+			this.bakeryLock.unlock();
+		}
+	}
+}
