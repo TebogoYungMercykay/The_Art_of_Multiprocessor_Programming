3cf2211d03b17d5aaad419f1adfaca984171bc0e
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-08-10 21:49:44 +0200

    This Is the Testing main for the Bakery Lock

---
+++ Codes/Bakery_Lock---N_Threads/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+	    MyThread[] threads = new MyThread[5];
+
+        SharedResources CriticalSection = new SharedResources();
+
+        for(int i = 0; i < 5; i++)
+            threads[i] = new MyThread(CriticalSection);
+
+        for(MyThread t : threads)
+            t.start();
+
+    }
+}
