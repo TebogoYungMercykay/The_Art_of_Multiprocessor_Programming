989b7619778b1561b66f55ffc0bbd24ea9bbfb3e
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-09-04 06:09:48 +0200

    This Class represents the Printer Used

--- Codes/MCS_Queue_Lock/Printer.java
+++ Codes/MCS_Queue_Lock/Printer.java
@@ -1,9 +1,25 @@
+import java.util.Random;
 import java.util.concurrent.locks.Lock;
 
 public class Printer {
-    Lock l;
+    Lock lock;
+    MCSQueue queue = new MCSQueue(); // Either MCSQueue or Timeout
 
-	public void Print(){
+    public Printer(Lock l) {
+        this.lock = l;
+    }
 
-	}
+    public void Print(int requestNumber) {
+        System.out.println(Thread.currentThread().getName() + ":Request-" + requestNumber + " printing request: ");
+        try {
+            Random rand = new Random();
+            int sleepTime = rand.nextInt(801) + 200;
+            Thread.sleep(sleepTime);
+        } catch(Exception exception){
+			exception.printStackTrace();
+		} finally {
+            System.out.print("QUEUE: ");
+            System.out.println(this.queue.requestInfoString(requestNumber));
+        }
+    }
 }
