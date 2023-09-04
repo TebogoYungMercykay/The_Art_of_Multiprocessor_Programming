7ef3ad7483daad017714f040834495c2ccdca391
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-09-04 06:45:52 +0200

    Class: Uses Time-out_Lock to Handle Print Req.

--- Codes/Time-out_Lock/Node.java
+++ Codes/Time-out_Lock/Node.java
@@ -1,15 +1,48 @@
+import java.util.Random;
+
 public class Node extends Thread {
+    private Printer printer;
+    int atLeastPrint = 5;
 
-    private Printer p;
+    public Node(Printer p) {
+        this.printer = p;
+    }
 
-	Node(Printer _p)
-	{
-		this.p = _p ;
-	}
+    @Override
+    public void run() {
+        // Simulate sending 5 print requests
+        for (int i = 1; i <= this.atLeastPrint; i++) {
+            try {
+                String message = generateRandomMessage(5 + (2 * i));
+                System.out.println("[" + Thread.currentThread().getName() + "][Request " + i + "] printing " + message);
+                this.printer.lock.lock();
+                this.printer.Print(i);
+            } catch (Exception exception) {
+                exception.printStackTrace();
+            } finally {
+                this.printer.lock.unlock();
+            }
+        }
+    }
 
-	@Override
-	public void run()
-	{
+    public static String generateRandomMessage(int length) {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String smallCharacters = "abcdefghijklmnopqrstuvwxyz";
+        StringBuilder randomString = new StringBuilder();
 
-	}
+        Random random = new Random();
+        for (int i = 0; i < length; i++) {
+            if (i == 0) {
+                int index = random.nextInt(characters.length());
+                char randomChar = characters.charAt(index);
+                randomString.append(randomChar);
+            } else {
+                int index = random.nextInt(smallCharacters.length());
+                char randomChar = smallCharacters.charAt(index);
+                randomString.append(randomChar);
+            }
+        }
+
+        return randomString.toString();
+    }
 }
