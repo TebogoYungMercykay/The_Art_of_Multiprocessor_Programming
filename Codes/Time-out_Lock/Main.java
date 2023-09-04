bd21d8e6fc8e3fa037a5a799cb2079cab2ba5a17
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-09-04 06:44:01 +0200

    Testing Main for Timeout_Lock Implementation

--- Codes/Time-out_Lock/Main.java
+++ Codes/Time-out_Lock/Main.java
@@ -1,5 +1,19 @@
 public class Main {
-    public static void main(String args[]){
-        
+    public static void main(String[] args) {
+        // Initialize the printer with either MCSQueue or Timeout
+        Timeout lock = new Timeout(); // Change to Timeout if needed
+        Printer printer = new Printer(lock);
+
+        // Create and start nodes
+        Node[] Node = new Node[5];
+        for(int i = 0; i < 5; i++)
+        {
+            Node[i] = new Node(printer);
+        }
+
+        for(Node Node2: Node)
+        {
+            Node2.start();
+        }
     }
 }
