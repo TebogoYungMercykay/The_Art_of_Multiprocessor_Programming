17562c1e766410d63b26f0ee9df5e900c21d76de
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-09-05 21:55:29 +0200

    Partial Implementation for the CRUD API

--- Codes/CRUD_API_Using_Multithreading/Crud.java
+++ Codes/CRUD_API_Using_Multithreading/Crud.java
@@ -1,19 +1,11 @@
 import java.util.LinkedList;
 import java.util.Queue;
-import java.util.Random;
-import java.util.concurrent.locks.Lock;
 
 public class Crud {
-    private volatile Queue<Info> database = new LinkedList<>();
     private volatile Queue<Info> create = new LinkedList<>();
     private volatile Queue<Boolean> read = new LinkedList<>();
     private volatile Queue<Info> update = new LinkedList<>();
     private volatile Queue<Info> delete = new LinkedList<>();
-
-    private Bakery createLock = new Bakery(1);
-    private Bakery readLock = new Bakery(1);
-    private Bakery updateLock = new Bakery(1);
-    private Bakery deleteLock = new Bakery(1);
 
     public Crud() {
         String ids[] = {"u123", "u456", "u789", "u321", "u654", "u987", "u147", "u258", "u369", "u741", "u852", "u963"};
@@ -30,106 +22,35 @@ public class Crud {
         }
     }
 
-    public void start() {
-        Thread createThread = new Thread(new Operator(create, database, 'c', createLock), "CreateThread");
-        Thread readThread = new Thread(new Operator(read, database, 'r', readLock), "ReadThread");
-        Thread updateThread = new Thread(new Operator(update, database, 'u', updateLock), "UpdateThread");
-        Thread deleteThread = new Thread(new Operator(delete, database, 'd', deleteLock), "DeleteThread");
+    public void CreateQueue() {
+		// return this.create;
+	}
 
-        createThread.start();
-        readThread.start();
-        updateThread.start();
-        deleteThread.start();
-    }
+    public void ReadQueue() {
+		// return this.read;
+	}
 
-    public class Operator implements Runnable {
-        private Queue<?> requests;
-        private Queue<Info> database;
-        private char operation;
-        private Random random;
-        private Lock lock;
+    public void UpdateQueue() {
+		// return this.update;
+	}
 
-        public Operator(Queue<?> requests, Queue<Info> database, char operation, Lock lock) {
-            this.requests = requests;
-            this.database = database;
-            this.operation = operation;
-            this.random = new Random();
-            this.lock = lock;
-        }
+    public void DeleteQueue() {
+		// return this.delete;
+	}
 
-        @Override
-        public void run() {
-            while (!requests.isEmpty()) {
-                System.out.println(Thread.currentThread().getName() + " is waiting for request.");
-                lock.lock();
-                try {
-                    switch (operation) {
-                        case 'c':
-                            Info createRequest = (Info) ((LinkedList<?>) requests).poll();
-                            database.add(createRequest);
-                            System.out.println(Thread.currentThread().getName() + " success [id=" + createRequest.id + ", name=" + createRequest.name + "]");
-                            break;
-                        case 'r':
-                            ((LinkedList<?>) requests).poll();
-                            System.out.println(Thread.currentThread().getName());
-                            System.out.println("—————————");
-                            for (Info record : database) {
-                                System.out.println("[id=" + record.id + ", name=" + record.name + ", practicals=" + record.practicals + ", assignments=" + record.assignments + "]");
-                            }
-                            System.out.println("—————————");
-                            break;
-                        case 'u':
-                            Info updateRequest = (Info) ((LinkedList<?>) requests).poll();
-                            boolean updated = false;
-                            for (Info record : database) {
-                                if (record.id.equals(updateRequest.id) && record.name.equals(updateRequest.name)) {
-                                    record.practicals = updateRequest.practicals;
-                                    record.assignments = updateRequest.assignments;
-                                    updated = true;
-                                    break;
-                                }
-                            }
-                            if (updated) {
-                                System.out.println(Thread.currentThread().getName() + " success [id=" + updateRequest.id + ", name=" + updateRequest.name + "]");
-                            } else {
-                                updateRequest.attempt++;
-                                if (updateRequest.attempt <= 2) {
-                                    ((LinkedList<Info>) requests).offer(updateRequest);
-                                }
-                                System.out.println(Thread.currentThread().getName() + " failed [id=" + updateRequest.id + ", name=" + updateRequest.name + "]");
-                            }
-                            break;
-                        case 'd':
-                            Info deleteRequest = (Info) ((LinkedList<?>) requests).poll();
-                            boolean deleted = false;
-                            for (Info record : database) {
-                                if (record.id.equals(deleteRequest.id) && record.name.equals(deleteRequest.name)) {
-                                    database.remove(record);
-                                    deleted = true;
-                                    break;
-                                }
-                            }
-                            if (deleted) {
-                                System.out.println(Thread.currentThread().getName() + " success [id=" + deleteRequest.id + ", name=" + deleteRequest.name + "]");
-                            } else {
-                                deleteRequest.attempt++;
-                                if (deleteRequest.attempt <= 2) {
-                                    ((LinkedList<Info>) requests).offer(deleteRequest);
-                                }
-                                System.out.println(Thread.currentThread().getName() + " failed [id=" + deleteRequest.id + ", name=" + deleteRequest.name + "]");
-                            }
-                            break;
-                    }
-                } finally {
-                    lock.unlock();
-                }
-                try {
-                    System.out.println(Thread.currentThread().getName() + " is sleeping.");
-                    Thread.sleep(random.nextInt(51) + 50);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
-    }
+    public Queue<Info> getCreateQueue() {
+		return this.create;
+	}
+
+    public Queue<Boolean> getReadQueue() {
+		return this.read;
+	}
+
+    public Queue<Info> getUpdateQueue() {
+		return this.update;
+	}
+
+    public Queue<Info> getDeleteQueue() {
+		return this.delete;
+	}
 }
