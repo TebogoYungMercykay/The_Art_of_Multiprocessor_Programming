07cbc5de57a79cda8eacf40264bf28206d24d363
Selepe Sello <sbkskhalo.kq@gmail.com>
2023-10-30 14:18:59 +0200

    The Job Requests Inserted into the central Database

---
+++ Codes/Unbound_Lock-Free_Stack/Job.java
@@ -0,0 +1,11 @@
+class Job {
+    int id;
+    int hours;
+    boolean isLastJob;
+
+    public Job(int id, int hours, boolean isLastJob) {
+        this.id = id;
+        this.hours = hours;
+        this.isLastJob = isLastJob;
+    }
+}
